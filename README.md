95d387c4b35ec1c85aaae229d46d28e25bab00c6
BRPersonal <balajirengan@gmail.com>
2024-11-05 18:25:01 +0530

    second version

--- README.md
+++ README.md
@@ -7,20 +7,30 @@ https://www.youtube.com/watch?v=Z1RJmh_OqeA&list=WL&index=11
 
 open a terminal
 #Install virtual env
-pip3 install virtualenv
+$ pip install virtualenv
 
-#Create virtual environment. this will create  env directory
-virtualenv env
+#Open a terminal in project root directory and run this command
+#This will Create virtual environment. This will create  env directory
+$ virtualenv env
 
 #activate virtual environment
-source env/bin/activate
+$ source env/bin/activate
 
 You will get into env prompt. Now you are inside virtual environment
 Now after this, anything that you install is contained within this env
 and will not install globally on your system. Your system is shielded from this env
 
-Now go ahead and install flask and alchemy
-pip3 install flask flask-sqlalchemy
+Install the necessary dependencies in this virtual environment.
+(env) is a prompt that indicates you are in virtual environment. It is
+not part of the command
+$ (env) pip install -r requirements.txt
+
+To check what versions are installed in virtual environment run
+$ pip list
+
+
+Finally start the web server:
+$ (env) python app.py
 
 sqllite 3 forward-slashes is relative path, four forward-slashes is absolute path
 source env/bin/activate
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
 
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db = SQLAlchemy(app)
 
+#This is an entity representing a table
+#id is automatically generated for every new record
 class Todo(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     content = db.Column(db.String(200), nullable=False)
@@ -16,12 +18,36 @@ class Todo(db.Model):
         return f"<Task {self.id}>"
 
 
-@app.route("/hello")
+@app.route("/",methods=["POST","GET"])
 def index() -> str:
-    return render_template("index.html")
+    if request.method == 'POST':
+        task = request.form["content"]
+        new_task = Todo(content=task)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue adding your task"
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html",tasks=tasks)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "There was a problem deleting that task"
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all() #create database tables
 
-    app.run(debug=True)
+    app.run(port=5001,debug=True)
